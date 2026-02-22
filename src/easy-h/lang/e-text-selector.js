d473f6c71d2ad184f08b721b7dda8c6159cf0388
EasyH <skysea001010@gmail.com>
2026-02-23 03:21:59 +0900

    ios 사파리에서 이동 편의 기능 디자인 오류 수정

--- src/easy-h/lang/e-text-selector.js
+++ src/easy-h/lang/e-text-selector.js
@@ -1,10 +1,11 @@
 import * as lang from "./e-text.js"
 
 class ETextSelect extends HTMLElement {
-
     constructor() {
         super();
         this.select = document.createElement('select');
+        this._hiddenOptions = []; // 숨겨진 옵션들을 임시 보관할 저장소
+
         ['value', 'selectedIndex', 'disabled'].forEach(prop => {
             Object.defineProperty(this, prop, {
                 get: () => this.select[prop],
@@ -13,25 +14,22 @@ class ETextSelect extends HTMLElement {
         });
     }
 
-    get value() {
-        return this.select.value;
-    }
-
     onChange() {
-        this.select.querySelectorAll('option[data-lang-key]').forEach(opt => {
+        // 기존 옵션들과 보관 중인 숨겨진 옵션들 모두 번역 갱신
+        const allOptions = [...this.select.options, ...this._hiddenOptions];
+        allOptions.forEach(opt => {
             const key = opt.getAttribute('data-lang-key');
             if (key) opt.text = lang.getTranslation(key);
         });
     }
 
     connectedCallback() {
-        if (this.querySelector('select')) return; // 이미 있으면 중단
+        if (this.contains(this.select)) return;
 
-        // 기존에 HTML에 적어둔 <option>들을 select 안으로 옮김
         while (this.firstChild) {
             this.select.appendChild(this.firstChild);
         }
-        this.select.id = this.id + "-select";
+        this.select.id = `${this.id}-select`;
         this.appendChild(this.select);
         lang.addObserver(this);
     }
@@ -40,7 +38,6 @@ class ETextSelect extends HTMLElement {
         lang.removeObserver(this);
     }
 
-    // 옵션을 추가할 때 사전을 참조하여 텍스트만 주입
     addOption(key, value) {
         const opt = document.createElement('option');
         opt.value = value;
