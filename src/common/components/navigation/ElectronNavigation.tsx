697141ad138450e74a7b47a997ab5e15ef41f115
EasyH <skysea001010@gmail.com>
2026-05-19 16:59:11 +0900

    github 경로 오류 수정

--- src/common/components/navigation/ElectronNavigation.tsx
+++ src/common/components/navigation/ElectronNavigation.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, ChangeEvent } from 'react';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { NavButton } from './WebNavigation';
-import Select from '../ui-brick/react-web/common/Select';
 
 export const FEATURES = [
     { label: '자세', value: '/pose' },
