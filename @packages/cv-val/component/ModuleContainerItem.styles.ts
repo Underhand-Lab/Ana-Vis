1b46dd62bedf47a9695329577d8e84b37eb1f90f
EasyH <skysea001010@gmail.com>
2026-06-12 16:23:07 +0900

    설정 패널 위치 변경

--- @packages/cv-val/component/ModuleContainerItem.styles.ts
+++ @packages/cv-val/component/ModuleContainerItem.styles.ts
@@ -38,7 +38,7 @@ export const moduleItemStyles: { [key: string]: React.CSSProperties } = {
   sidePanel: {
     position: 'absolute',
     top: 0,
-    right: 0,
+    left: 0,
     bottom: 0,
     padding: '20px',
     zIndex: 20,
